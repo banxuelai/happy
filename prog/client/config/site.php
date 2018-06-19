1b2a9ffe25a60e83ed3b9f7eb1423ed6a2c11965
banxuelai160714 <banxuelai>
2018-06-19 17:00:25 +0800

    just for del client and morr

--- prog/client/config/site.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'title' => '智博继续教育',
-    'static_base' => 'http://47.104.90.191/static',
-    'base_url' => '//'.$_SERVER['HTTP_HOST'],
-    'log_dir' => '/tmp/data/aducode',
-);
