5768a8d9ecfd96955dfeeacde9ceff0faba767db
banxuelai160714 <banxuelai>
2018-06-25 10:56:43 +0800

    fixed

--- prog/admin/config/dev/site.php
+++ prog/admin/config/dev/site.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'title' => '智博继续教育',
+    'title' => '乐活网',
     'static_base' => 'http://47.104.93.177/static',
     'base_url' => '//'.$_SERVER['HTTP_HOST'],
     'log_dir' => '/tmp/data/aducode',
