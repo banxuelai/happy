92692a61c18f8438adf1c6b8fd9d23be32a902c0
banxuelai160714 <banxuelai>
2018-06-26 16:54:28 +0800

    client/lists

---
+++ prog/admin/controller/ClientController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 前端展示
+ * @author bxl@gmail.com
+ * @date 2018-06-23
+ *
+ */
+class ClientController extends Controller
+{
+	/**
+	 * @desc 前端展示项目列表
+	 */
+	public function lists()
+	{
+		$project_model = new ProjectModel();
+		
+		$cond = array(
+			'project_status' => 1,
+		);
+		
+		# 获取列表
+		$projectList = $project_model->getList($cond,-1);
+		
+		$this->display('client/lists.html', array(
+			'project_list' => $projectList,
+		));
+	}
+}
